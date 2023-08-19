e474c166c100c4a0cfd4fa74a5c80fa4da947c00
Spannos <cspannos@blockdaemon.com>
2023-08-19 23:01:42 +0100

    changed filename

---
+++ solidity/contracts/isms/blockhash/BlockHashIsm.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity >=0.8.0;
+
+// ============ Internal Imports ============ //
+import {IBlockHashIsm} from "../../interfaces/isms/IBlockHashIsm.sol";
+import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
+import {IMailbox} from "../../interfaces/IMailbox.sol";
+import {Message} from "../../libs/Message.sol";
