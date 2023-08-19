43a8e0a1472f783fdf6577b397572038ded0e6cc
Spannos <cspannos@blockdaemon.com>
2023-08-19 23:07:12 +0100

    imported assumed oracle

--- solidity/contracts/isms/blockhash/BlockHashIsm.sol
+++ solidity/contracts/isms/blockhash/BlockHashIsm.sol
@@ -6,3 +6,9 @@ import {IBlockHashIsm} from "../../interfaces/isms/IBlockHashIsm.sol";
 import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
 import {IMailbox} from "../../interfaces/IMailbox.sol";
 import {Message} from "../../libs/Message.sol";
+   /** 
+    * @notice This project assumes a magic onchain oracle. The oracle imported below dos not exist
+    */
+import {Oracle} from "../../libs/Oracle.sol";
+
+
--- solidity/contracts/isms/blockhash/BlockHashOracleIsm.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.8.0;
-
-// ============ Internal Imports ============ //
-import {IBlockHashIsm} from "../../interfaces/isms/IBlockHashIsm.sol";
-import {IInterchainSecurityModule} from "../../interfaces/IInterchainSecurityModule.sol";
-import {IMailbox} from "../../interfaces/IMailbox.sol";
-import {Message} from "../../libs/Message.sol";
