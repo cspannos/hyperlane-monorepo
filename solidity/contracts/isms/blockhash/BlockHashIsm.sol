d7ec4b5bdf532f07d5c33098d76acde25aaddc1d
Spannos <cspannos@blockdaemon.com>
2023-08-19 23:41:06 +0100

    added oracle timeout

--- solidity/contracts/isms/blockhash/BlockHashIsm.sol
+++ solidity/contracts/isms/blockhash/BlockHashIsm.sol
@@ -25,7 +25,8 @@ abstract contract BlockHashISM is IBlockHashIsm, Ownable {
     mapping(address => bool) public oracles; //oracles added by owner
     mapping(address => bytes) public _oraclesToBlockHash; //oracles to blockhash mapping
 
-
+    // ============ Oracle Timeout Variables ============
+    uint256 public oracleTimeout; //duration set by owner in deployment OR after via changeOracleTimeout() for throwing error
 
 
 
