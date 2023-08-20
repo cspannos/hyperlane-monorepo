bc0619c965c4de7137dc778158e5855e71f3d142
Spannos <cspannos@blockdaemon.com>
2023-08-20 22:28:33 +0100

    added chain_id to struct

--- solidity/contracts/isms/blockhash/MessageDispatched.sol
+++ solidity/contracts/isms/blockhash/MessageDispatched.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.8.0;
 
 // ============ STRUCT ============
     struct MessageDispatched {
+        uint256 chain_id;
         uint256 block_hash; 
         uint256 parent_hash; 
         uint256 attestation_signatures; 
